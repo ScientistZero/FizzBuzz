a70592a032a703aea0db6a687cf196e852c7dc59
Chris Herzig <81333676+ScientistZero@users.noreply.github.com>
2023-09-26 16:15:23 -0400

    Finished Js functions

--- js/site.js
+++ js/site.js
@@ -6,106 +6,50 @@ function getValues(){
     let buzzValue = document.getElementById("buzzValue").value;
     let numbers = [];
     //parse for numbers.
-
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
     //check they are integers.
+    if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)){
+        //we can call fizzBuzz
+        let fbArray = fizzBuzz(fizzValue,buzzValue);
+        //call displayData and write them to the screen.
+        displayData(fbArray);
 
-    //we can call fizzBuzz
-
-    //call displayData and write them to the screen.
-
-
+        }else{
+    alert("You must enter intgers");
+    
+        }
 }
 
-
-function fizzBuzz(fizzValue, buzzValue)
-{
-
-    // init the returnArray
+    //do Fizz Buzz
+function fizzBuzz(fizzValue, buzzValue){
     let returnArray = [];
-
     //loop from 1 to 100
+    for (let i = 1; i <= 100; i++) {
+        
+        //check to see if divisible by both 3 and 5
+        //check to see if divisible by Fizz value (3)
+        //check to see if divisible by Buzz value (5)
+        if (i % fizzvalue ==0 && i % buzzValue == 0) {
+            returnArray.push('FizzBuzz');
+        } else if (i % fizzValue == 0){
+            returnArray.push('Fizz');
+        } else if (i % buzzValue == 0){
+            returnArray.push('Buzz');
+        } else {
+            returnArray.push(i);
+        }
+    }
 
-    //we need to check the current in three steps
-    //check to see if divisible by both 3 and 5
-    //if so push 'FizzBuzz' into array and not the number
-
-    //check to see if divisible by Fizz value (3)
-    //if so push 'Fizz' into array and not the number
-
-    //check to see if divisible by Buzz value (5)
-    //if so push 'Buzz' into array and not the number
-
-    //finally, if none then push the number into the array
 
     returnArray;
 
 }
 
+    //loop over the array and create a tablerow for each item
 function displayData(fbArray){
 
-    //loop over the array and create a tablerow for each item
+    
 
     //add all the rows to the table
-}
-
-
-//OLD CODE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//get the values from the page
-//start or controller function
-function getValues() {
-    //get values from page
-let fizzValue = document.getElementById("fizzValue").value;
-let buzzValue = document.getElementById("buzzValue").value;
-let numbers = [];
-//We need to validate our input
-//parse into integers
-fizzValue = parseInt(fizzValue);
-buzzValue = parseInt(buzzValue);
-
-if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)){
-
-    //call generateNumbers
-    let numbers = generateNumbers(fizzValue,buzzValue);
-     //call displayNumbers
-     displayNumbers(numbers);
-    }else{
-alert("You must enter intgers");
-
-    }
-
-
-   
-}
-
-//generate numbers from the startValue to the endValue
-//logic functions
-function generateNumbers(fValue, bValue){
-    let numbers = [];
-
-    //we want to get all numbers from start to end
-for (let index = fValue; index <= bValue; index++) {
-    //this will execute in a loop until index = eValue
-    numbers.push(index);
-}
-    return numbers;
-}
-
-//display the numbers and mark the even numbers bold
-//display or view functions
-function displayNumbers(numbers){
-let templateRows = "";
-    for (let index = 0; index < numbers.length; index++) {
-        let className = "even";
-        let number = numbers[index];
-        if(number % 2 == 0){
-            className = "even";
-
-        }
-        else{
-className = "odd";
-        }
-        templateRows += `<tr><td class="${className}">${number}</td></tr>`;
-    }
-document.getElementById('results').innerHTML = templateRows
-
 }

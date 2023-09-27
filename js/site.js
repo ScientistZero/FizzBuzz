e403d9e65a6e513789d1e035786aa516cc12231e
Chris Herzig <81333676+ScientistZero@users.noreply.github.com>
2023-09-27 10:12:15 -0400

    Working on Code page

--- js/site.js
+++ js/site.js
@@ -33,10 +33,13 @@ function fizzBuzz(fizzValue, buzzValue){
         //check to see if divisible by Buzz value (5)
         if (i % fizzValue == 0 && i % buzzValue == 0) {
             returnArray.push('FizzBuzz');
+
         } else if (i % fizzValue == 0){
             returnArray.push('Fizz');
+
         } else if (i % buzzValue == 0){
             returnArray.push('Buzz');
+            
         } else {
             returnArray.push(i);
         }
@@ -63,6 +66,7 @@ function displayData(fbArray){
         let tableRow = document.importNode(templateRow.content, true);
 
         //grab just the td's to put into array
+        //add all the rows to the table
         let rowCols = tableRow.querySelectorAll("td");
         rowCols[0].classList.add(fbArray[index]);
         rowCols[0].textContent = fbArray[index];
@@ -79,5 +83,5 @@ function displayData(fbArray){
         
     }
 
-    //add all the rows to the table
+    
 }
